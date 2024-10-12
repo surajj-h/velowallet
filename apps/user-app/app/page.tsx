245d4082e3c0ddc8348f47114f6618d6ab76356f
suraj <surajhiremath7@gmail.com>
2024-10-12 12:23:14 +0530

    init commit

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      User app
+    </div>
+  );
+}
