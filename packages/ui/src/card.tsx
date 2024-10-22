f95d3fea001836efbc95e5630ec4320def833d51
suraj <surajhiremath7@gmail.com>
2024-10-22 10:05:59 +0530

    Add ui components

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -1,25 +1,20 @@
-export function Card({
-  className,
+import React from "react";
+
+export const Card = ({
   title,
   children,
-  href,
 }: {
-  className?: string;
   title: string;
-  children: React.ReactNode;
-  href: string;
-}): JSX.Element {
+  children?: React.ReactNode;
+}) => {
   return (
-    <a
-      className={className}
-      href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
-      rel="noopener noreferrer"
-      target="_blank"
+    <div
+      className="border p-4"
     >
-      <h2>
-        {title} <span>-&gt;</span>
-      </h2>
-      <p>{children}</p>
-    </a>
+      <h1 className="text-xl border-b pb-2">
+        {title}
+      </h1>
+      {children}
+    </div>
   );
 }
