6dad40984318b0529befcdae8b1d04b181291ea3
Suraj H <surajhiremath7@gmail.com>
2024-10-24 22:15:54 +0530

    Delete docker-entrypoint.sh

--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Generate Prisma client at runtime when DATABASE_URL is available
-echo "Running Prisma generate..."
-npm run db:migrate
-
-# Execute the main container command
-exec "$@"
